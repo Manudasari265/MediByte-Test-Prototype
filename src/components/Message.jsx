262620b391adbfbbc3c611f5eafbbaeeab4c57e2
Dasari Manoj <122810482+Manudasari265@users.noreply.github.com>
2024-05-19 13:08:03 +0530

    Components added

---
+++ src/components/Message.jsx
@@ -0,0 +1,13 @@
+// import { companyLogos } from "../constants";
+
+const Message = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create a healthy Lifestyle
+      </h5>
+    </div>
+  );
+};
+
+export default Message;
